0ec444a75b691a55fd6fe671214378fb8054c4c9
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-31 18:49:26 +0300

    Update index.pl

--- index.pl
+++ index.pl
@@ -4,12 +4,13 @@ use strict;
 use warnings;
 use CGI;
 
-sub readfile {
+sub getfile {
     my $filename = $_[0];
     my $file;
     my $res = "";
     if (!open($file, "<:encoding(UTF-8)", $filename)) {
-        die "Could not open file '$filename' $!";
+        print "Could not open file '$filename' $!";
+        return "";
     }
     while (my $s = <$file>) {
         $res .= $s;
@@ -18,22 +19,36 @@ sub readfile {
     return $res;
 }
 
-sub main {
-    print("Content-type: text/html\n\n");
-
-    my $query = CGI->new;
-    my $val = $query->param("answer");
-
-    #print readfile("dummy.html");
-
-    print "<html>";
-    print     "<head>";
-    print         "<title>Sent</title>";
-    print     "</head>";
-    print     "<body>";
-    print         "<pre>answer: $val</pre>";
-    print     "</body>";
-    print "</html>";
+sub putfile {
+    my ($filename, $text) = @_;
+    my $file;
+    if (!open($file, ">:encoding(UTF-8)", $filename)) {
+        print "Could not open file '$filename' $!";
+        return;
+	}
+    print $file $text;
+    close($file);
 }
 
-exit(main());
+sub cur_date 
+    { return `date +%d.%m.%Y`; }
+sub cur_time
+    { return `date +%H:%M`   ; }
+
+sub main {
+    print("Content-type: text/html\n\n");
+    my $query = CGI->new;
+    my $text = $query->param("post");
+    my $post = getfile("base.html");
+    if ($text ne "") {
+        $post .= "_" x 4 . " " . cur_date() . " " . cur_time() . " " . "_" x 20;
+        $post .= "<pre>" . $text . "</pre><hr>";
+        putfile("base.html", $post);
+    }
+    my $html = getfile("book.html");
+    $html =~ s/{}/$post/;
+    print $html;
+    #print(sprintf($html, $post));  ## if i will change '{}' to '%s' in book.html
+    return 0;
+}
+exit(main(@ARGV));
